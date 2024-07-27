a76afd86d399b9af4d9e68c19fd973bbc32fd188
Emre <emre05ceylan9@gmail.com>
2024-07-27 22:27:38 +0300

    themes indexed

--- src/app/context/appcontext.js
+++ src/app/context/appcontext.js
@@ -1,14 +1,13 @@
 import { createContext, useContext } from "react";
 import { useState } from "react";
-import darkTheme from '../themes/variants/dark/index.js';
-import lightTheme from '../themes/variants/light/index.js';
+import themes from '../themes';
 const Context = createContext();
 const Provider = ({ children }) => {
 
 	const [theme, setTheme] = useState("light");
 	const [language, setLanguage] = useState("tr");
 	const [isMenuOpen, setIsMenuOpen] = useState("closed");
-	const currentTheme = theme === 'dark' ? darkTheme : lightTheme;
+	const currentTheme = themes[theme];
 
 	const toggleTheme =()=>{
 		setTheme(theme === "light" ? "dark" : "light");
---
+++ src/app/themes/index.js
@@ -0,0 +1,9 @@
+import darkTheme from "./variants/dark";
+import lightTheme from "./variants/light";
+
+const themes = {
+    dark: darkTheme,
+    light: lightTheme
+};
+
+export default themes;
